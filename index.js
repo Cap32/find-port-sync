edb32cc2aff3a22f4b6e0bae38b664d16c91c8c7
3went2 <webb.png@gmail.com>
2017-08-17 13:13:09 +0800

    make compatible with windows

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = () => +require('child_process').execSync(require.resolve('get-port-cli/cli'))
+module.exports = () => +require('child_process').execSync(`${JSON.stringify(process.execPath)} ${require.resolve('get-port-cli/cli')}`)
